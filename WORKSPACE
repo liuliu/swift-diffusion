f0ff365a37f56ecb9a606e2f213d59631c63f926
Liu Liu <i@liuliu.me>
2023-06-29 19:29:41 -0400

    Add quantization model support.

--- WORKSPACE
+++ WORKSPACE
@@ -3,9 +3,9 @@ load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
 
 git_repository(
     name = "s4nnc",
-    commit = "371d8a761babe8dfc655b7f6303358e43726b970",
+    commit = "47662136d1d8276b600183c51a16bcbe2bfc029f",
     remote = "https://github.com/liuliu/s4nnc.git",
-    shallow_since = "1683238560 -0400",
+    shallow_since = "1688081329 -0400",
 )
 
 load("@s4nnc//:deps.bzl", "s4nnc_deps")
---
+++ examples/q6p/main.swift
@@ -0,0 +1,32 @@
+import NNC
+
+let graph = DynamicGraph()
+
+graph.openStore(
+  "/home/liu/workspace/swift-diffusion/realistic_vision_v3_0_f16.ckpt", flags: .truncateWhenClose
+) { store in
+  let keys = store.keys
+  graph.openStore(
+    "/home/liu/workspace/swift-diffusion/realistic_vision_v3.0_q6p_q8p.ckpt",
+    flags: .truncateWhenClose
+  ) {
+    for key in keys {
+      guard let tensor = store.read(key) else { continue }
+      let shape = tensor.shape
+      print("write \(key) \(tensor)")
+      var n = 0
+      for i in 0..<shape.count {
+        if shape[i] > 1 {
+          n += 1
+        }
+      }
+      if shape.count == 2 && n > 1 {
+        $0.write(key, tensor: tensor, codec: [.q6p, .ezm7])
+      } else if shape.count == 4 && n > 1 {
+        $0.write(key, tensor: tensor, codec: [.q8p, .ezm7])
+      } else {
+        $0.write(key, tensor: tensor, codec: [.ezm7])
+      }
+    }
+  }
+}
