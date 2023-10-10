89a1edd5c94626a05625d0ab108723f79b2e66e6
Liu Liu <i@liuliu.me>
2023-10-10 15:49:42 -0400

    Add OPT model to BLIP2.

--- examples/blip2/main.swift
+++ examples/blip2/main.swift
@@ -20,12 +20,12 @@ let (model, vis_processors, _) = lavis.models.load_model_and_preprocess(
 
 let image = vis_processors["eval"](raw_image).unsqueeze(0).to(device)
 print(image.shape)
-print(model.Qformer.bert)
-let result = model.generate(["image": image])
+print(model.opt_model)
+let result = model.generate(["image": image, "prompt": "a photo of"])
 let state_dict = model.visual_encoder.state_dict()
 let model_state_dict = model.state_dict()
 let bert_state_dict = model.Qformer.bert.state_dict()
-print(bert_state_dict.keys())
+let opt_state_dict = model.opt_model.state_dict()
 print(result)
 
 func EvaSelfAttention(prefix: String, k: Int, h: Int, b: Int, t: Int) -> (
@@ -344,6 +344,154 @@ func BertModel(
   return (Model([x, c], [out]), reader)
 }
 
+func OPTSelfAttention(prefix: String, k: Int, h: Int, b: Int, t: Int) -> (
+  Model, (PythonObject) -> Void
+) {
+  let x = Input()
+  let causalAttentionMask = Input()
+  let tokeys = Dense(count: k * h)
+  let toqueries = Dense(count: k * h)
+  let tovalues = Dense(count: k * h)
+  let keys = tokeys(x).reshaped([b, t, h, k]).permuted(0, 2, 1, 3)
+  let queries = ((1.0 / Float(k).squareRoot()) * toqueries(x)).reshaped([b, t, h, k])
+    .permuted(0, 2, 1, 3)
+  let values = tovalues(x).reshaped([b, t, h, k]).permuted(0, 2, 1, 3)
+  var dot = Matmul(transposeB: (2, 3))(queries, keys) + causalAttentionMask
+  dot = dot.reshaped([b * h * t, t])
+  dot = dot.softmax()
+  dot = dot.reshaped([b, h, t, t])
+  var out = dot * values
+  out = out.reshaped([b, h, t, k]).transposed(1, 2).reshaped([b * t, h * k])
+  let unifyheads = Dense(count: k * h)
+  out = unifyheads(out)
+  let reader: (PythonObject) -> Void = { state_dict in
+    let q_weight = state_dict["\(prefix).self_attn.q_proj.weight"].type(torch.float).cpu()
+      .numpy()
+    toqueries.weight.copy(from: try! Tensor<Float>(numpy: q_weight))
+    let k_weight = state_dict["\(prefix).self_attn.k_proj.weight"].type(torch.float).cpu().numpy()
+    tokeys.weight.copy(from: try! Tensor<Float>(numpy: k_weight))
+    let v_weight = state_dict["\(prefix).self_attn.v_proj.weight"].type(torch.float).cpu()
+      .numpy()
+    tovalues.weight.copy(from: try! Tensor<Float>(numpy: v_weight))
+    let q_bias = state_dict["\(prefix).self_attn.q_proj.bias"].type(torch.float).cpu().numpy()
+    toqueries.bias.copy(from: try! Tensor<Float>(numpy: q_bias))
+    let k_bias = state_dict["\(prefix).self_attn.k_proj.bias"].type(torch.float).cpu().numpy()
+    tokeys.bias.copy(from: try! Tensor<Float>(numpy: k_bias))
+    let v_bias = state_dict["\(prefix).self_attn.v_proj.bias"].type(torch.float).cpu().numpy()
+    tovalues.bias.copy(from: try! Tensor<Float>(numpy: v_bias))
+    let proj_weight = state_dict["\(prefix).self_attn.out_proj.weight"].type(torch.float).cpu()
+      .numpy()
+    unifyheads.weight.copy(from: try! Tensor<Float>(numpy: proj_weight))
+    let proj_bias = state_dict["\(prefix).self_attn.out_proj.bias"].type(torch.float).cpu()
+      .numpy()
+    unifyheads.bias.copy(from: try! Tensor<Float>(numpy: proj_bias))
+  }
+  return (Model([x, causalAttentionMask], [out]), reader)
+}
+
+func OPTDecodeLayer(prefix: String, k: Int, h: Int, b: Int, t: Int, MLP: Int) -> (
+  Model, (PythonObject) -> Void
+) {
+  let x = Input()
+  let causalAttentionMask = Input()
+  let layerNorm1 = LayerNorm(epsilon: 1e-5, axis: [1])
+  var out = layerNorm1(x)
+  let (attention, attnReader) = OPTSelfAttention(prefix: prefix, k: k, h: h, b: b, t: t)
+  out = attention(out, causalAttentionMask) + x
+  let residual = out
+  let layerNorm2 = LayerNorm(epsilon: 1e-5, axis: [1])
+  out = layerNorm2(out)
+  let fc = Dense(count: MLP)
+  let relu = ReLU()
+  let proj = Dense(count: k * h)
+  out = residual + proj(relu(fc(out)))
+  let reader: (PythonObject) -> Void = { state_dict in
+    attnReader(state_dict)
+    let norm1_weight = state_dict["\(prefix).self_attn_layer_norm.weight"].type(torch.float)
+      .cpu().numpy()
+    let norm1_bias = state_dict["\(prefix).self_attn_layer_norm.bias"].type(torch.float).cpu()
+      .numpy()
+    layerNorm1.weight.copy(from: try! Tensor<Float>(numpy: norm1_weight))
+    layerNorm1.bias.copy(from: try! Tensor<Float>(numpy: norm1_bias))
+    let norm2_weight = state_dict["\(prefix).final_layer_norm.weight"].type(torch.float)
+      .cpu().numpy()
+    let norm2_bias = state_dict["\(prefix).final_layer_norm.bias"].type(torch.float).cpu()
+      .numpy()
+    layerNorm2.weight.copy(from: try! Tensor<Float>(numpy: norm2_weight))
+    layerNorm2.bias.copy(from: try! Tensor<Float>(numpy: norm2_bias))
+    let fc1_weight = state_dict["\(prefix).fc1.weight"].type(torch.float).cpu()
+      .numpy()
+    let fc1_bias = state_dict["\(prefix).fc1.bias"].type(torch.float).cpu()
+      .numpy()
+    fc.weight.copy(from: try! Tensor<Float>(numpy: fc1_weight))
+    fc.bias.copy(from: try! Tensor<Float>(numpy: fc1_bias))
+    let fc2_weight = state_dict["\(prefix).fc2.weight"].type(torch.float).cpu()
+      .numpy()
+    let fc2_bias = state_dict["\(prefix).fc2.bias"].type(torch.float).cpu().numpy()
+    proj.weight.copy(from: try! Tensor<Float>(numpy: fc2_weight))
+    proj.bias.copy(from: try! Tensor<Float>(numpy: fc2_bias))
+  }
+  return (Model([x, causalAttentionMask], [out]), reader)
+}
+
+public func OPTTextEmbedding<T: TensorNumeric>(
+  _ dataType: T.Type, batchSize: Int, vocabularySize: Int, maxLength: Int, embeddingSize: Int
+) -> (Model, (PythonObject) -> Void) {
+  let queryEmbed = Input()
+  let tokens = Input()
+  let positions = Input()
+  let tokenEmbed = Embedding(
+    T.self, vocabularySize: vocabularySize, embeddingSize: embeddingSize)
+  let positionEmbed = Embedding(T.self, vocabularySize: maxLength, embeddingSize: embeddingSize)
+  let embedding =
+    Functional.concat(axis: 0, queryEmbed, tokenEmbed(tokens)) + positionEmbed(positions)
+  let reader: (PythonObject) -> Void = { state_dict in
+    let vocab = state_dict["model.decoder.embed_tokens.weight"].type(torch.float).cpu().numpy()
+    let pos = state_dict["model.decoder.embed_positions.weight"].type(torch.float).cpu().numpy()
+    tokenEmbed.parameters.copy(from: try! Tensor<Float>(numpy: vocab))
+    positionEmbed.parameters.copy(from: try! Tensor<Float>(numpy: pos))
+  }
+  return (Model([queryEmbed, tokens, positions], [embedding], name: "embeddings"), reader)
+}
+
+func OPTDecoder<T: TensorNumeric>(
+  _ dataType: T.Type, vocabularySize: Int, maxLength: Int, width: Int, tokenLength: Int,
+  layers: Int, MLP: Int, heads: Int, batchSize: Int
+) -> (Model, (PythonObject) -> Void) {
+  let queryEmbed = Input()
+  let tokens = Input()
+  let positions = Input()
+  let (embedding, embedReader) = OPTTextEmbedding(
+    T.self, batchSize: batchSize, vocabularySize: vocabularySize, maxLength: maxLength,
+    embeddingSize: width)
+  var out = embedding(queryEmbed, tokens, positions)
+  let causalAttentionMask = Input()
+  var readers = [(PythonObject) -> Void]()
+  for i in 0..<layers {
+    let (layer, reader) = OPTDecodeLayer(
+      prefix: "model.decoder.layers.\(i)", k: width / heads, h: heads, b: batchSize, t: tokenLength,
+      MLP: MLP)
+    out = layer(out, causalAttentionMask)
+    readers.append(reader)
+  }
+  let finalLayerNorm = LayerNorm(epsilon: 1e-5, axis: [1])
+  out = finalLayerNorm(out)
+  let reader: (PythonObject) -> Void = { state_dict in
+    embedReader(state_dict)
+    for reader in readers {
+      reader(state_dict)
+    }
+    let final_layer_norm_weight = state_dict["model.decoder.final_layer_norm.weight"].type(
+      torch.float
+    ).cpu().numpy()
+    let final_layer_norm_bias = state_dict["model.decoder.final_layer_norm.bias"].type(torch.float)
+      .cpu().numpy()
+    finalLayerNorm.weight.copy(from: try! Tensor<Float>(numpy: final_layer_norm_weight))
+    finalLayerNorm.bias.copy(from: try! Tensor<Float>(numpy: final_layer_norm_bias))
+  }
+  return (Model([queryEmbed, tokens, positions, causalAttentionMask], [out]), reader)
+}
+
 let graph = DynamicGraph()
 let (vit, reader) = EvaVisionTransformer(
   grid: 26, width: 1408, MLP: 6144, layers: 39, heads: 16, batchSize: 1)
@@ -351,6 +499,9 @@ let ln_vision = LayerNorm(epsilon: 1e-5, axis: [1])
 let (qformer, qformerReader) = BertModel(
   width: 768, queryEmbeddingLength: 32, imageEmbeddingLength: 26 * 26 + 1, MLP: 768 * 4, layers: 12,
   heads: 12, batchSize: 1, crossAttentionFreq: 2)
+let (opt, optReader) = OPTDecoder(
+  Float.self, vocabularySize: 50272, maxLength: 2050, width: 2560, tokenLength: 36, layers: 32,
+  MLP: 10240, heads: 32, batchSize: 1)
 graph.withNoGrad {
   let xTensor = graph.variable(try! Tensor<Float>(numpy: image.cpu().numpy())).toGPU(0)
   vit.compile(inputs: xTensor)
@@ -369,5 +520,49 @@ graph.withNoGrad {
   qformer.compile(inputs: queryTokensTensor, out)
   qformerReader(bert_state_dict)
   out = qformer(inputs: queryTokensTensor, out)[0].as(of: Float.self)
-  debugPrint(out)
+  let optProj = Dense(count: 2560)
+  let opt_proj_weight = model_state_dict["opt_proj.weight"].type(torch.float).cpu().numpy()
+  let opt_proj_bias = model_state_dict["opt_proj.bias"].type(torch.float).cpu().numpy()
+  optProj.compile(inputs: out)
+  optProj.weight.copy(from: try! Tensor<Float>(numpy: opt_proj_weight))
+  optProj.bias.copy(from: try! Tensor<Float>(numpy: opt_proj_bias))
+  out = optProj(inputs: out)[0].as(of: Float.self)
+  let tokensTensor = graph.variable(.CPU, .C(4), of: Int32.self)
+  let positionTensor = graph.variable(.CPU, .C(36), of: Int32.self)
+  tokensTensor[0] = 2
+  tokensTensor[1] = 102
+  tokensTensor[2] = 1345
+  tokensTensor[3] = 9
+  for i in 0..<(4 + 32) {
+    positionTensor[i] = Int32(2 + i)
+  }
+  let causalAttentionMask = graph.variable(Tensor<Float>(.CPU, .NHWC(1, 1, 36, 36)))
+  causalAttentionMask.full(0)
+  for i in 0..<35 {
+    for j in (i + 1)..<36 {
+      causalAttentionMask[0, 0, i, j] = -Float.greatestFiniteMagnitude
+    }
+  }
+  let tokensTensorGPU = tokensTensor.toGPU(0)
+  let positionTensorGPU = positionTensor.toGPU(0)
+  let causalAttentionMaskGPU = causalAttentionMask.toGPU(0)
+  opt.compile(inputs: out, tokensTensorGPU, positionTensorGPU, causalAttentionMaskGPU)
+  optReader(opt_state_dict)
+  out = opt(inputs: out, tokensTensorGPU, positionTensorGPU, causalAttentionMaskGPU)[0].as(
+    of: Float.self)
+  let lastOut = out[35..<36, 0..<2560]
+  let lmHead = Dense(count: 50272, noBias: true)
+  lmHead.compile(inputs: lastOut)
+  let lm_head_weight = opt_state_dict["lm_head.weight"].type(torch.float).cpu().numpy()
+  lmHead.weight.copy(from: try! Tensor<Float>(numpy: lm_head_weight))
+  out = lmHead(inputs: lastOut)[0].as(of: Float.self).toCPU()
+  var maxIdx: Int = -1
+  var maxVal: Float = -Float.greatestFiniteMagnitude
+  for i in 0..<50272 {
+    if out[0, i] > maxVal {
+      maxVal = out[0, i]
+      maxIdx = i
+    }
+  }
+  print("next token: \(maxIdx)")
 }
