711a5581f0efde680ec084198ffa487a43e14265
Liu Liu <i@liuliu.me>
2024-07-18 13:14:17 -0400

    Done model conversion for AuraFlow. Need to test.

--- examples/auraflow/main.swift
+++ examples/auraflow/main.swift
@@ -234,7 +234,7 @@ let x = torch.randn([2, 4, 128, 128]).to(torch.float16).cuda()
 let y = torch.randn([2, 256, 2048]).to(torch.float16).cuda() * 0.01
 let t = torch.full([2], 1).cuda()
 
-pipeline.transformer(x, y, t)
+print(pipeline.transformer(x, y, t))
 
 let state_dict = pipeline.transformer.state_dict()
 
@@ -473,6 +473,103 @@ func JointTransformerBlock(
   }
 }
 
+func SingleTransformerBlock(
+  prefix: String, k: Int, h: Int, b: Int, t: Int, hw: Int, contextBlockPreOnly: Bool
+) -> ((PythonObject) -> Void, Model) {
+  let x = Input()
+  let c = Input()
+  let xAdaLNs = (0..<6).map { Dense(count: k * h, noBias: true, name: "x_ada_ln_\($0)") }
+  let xChunks = xAdaLNs.map { $0(c) }
+  let xNorm1 = LayerNorm(epsilon: 1e-5, axis: [2], elementwiseAffine: false)
+  let xOut = (1 + xChunks[1]) .* xNorm1(x) + xChunks[0]
+  let xToKeys = Dense(count: k * h, noBias: true, name: "x_k")
+  let xToQueries = Dense(count: k * h, noBias: true, name: "x_q")
+  let xToValues = Dense(count: k * h, noBias: true, name: "x_v")
+  var xK = xToKeys(xOut).reshaped([b, t + hw, h, k])
+  let normK = LayerNorm(epsilon: 1e-5, axis: [3], elementwiseAffine: false)
+  xK = normK(xK)
+  var xQ = xToQueries(xOut).reshaped([b, t + hw, h, k])
+  let normQ = LayerNorm(epsilon: 1e-5, axis: [3], elementwiseAffine: false)
+  xQ = normQ(xQ)
+  let xV = xToValues(xOut).reshaped([b, t + hw, h, k])
+  var keys = xK
+  var values = xV
+  var queries = xQ
+  // Now run attention.
+  keys = keys.permuted(0, 2, 1, 3)
+  queries = ((1.0 / Float(k).squareRoot()) * queries)
+    .permuted(0, 2, 1, 3)
+  values = values.permuted(0, 2, 1, 3)
+  var dot = Matmul(transposeB: (2, 3))(queries, keys)
+  dot = dot.reshaped([b * h * (t + hw), t + hw])
+  dot = dot.softmax()
+  dot = dot.reshaped([b, h, (t + hw), t + hw])
+  var out = dot * values
+  out = out.reshaped([b, h, (t + hw), k]).transposed(1, 2).reshaped([b, (t + hw), h * k])
+  var xIn: Model.IO = x
+  if contextBlockPreOnly {
+    out = out.reshaped([b, hw, h * k], offset: [0, t, 0], strides: [(t + hw) * h * k, h * k, 1])
+      .contiguous()
+    xIn = x.reshaped([b, hw, h * k], offset: [0, t, 0], strides: [(t + hw) * h * k, h * k, 1])
+      .contiguous()
+  }
+  let xUnifyheads = Dense(count: k * h, noBias: true, name: "x_o")
+  out = xUnifyheads(out)
+  out = xIn + xChunks[2] .* out
+  // Attentions are now. Now run MLP.
+  let (xLinear1, xLinear2, xOutProjection, xFF) = FeedForward(
+    hiddenSize: k * h, intermediateSize: k * h * 8 / 3, name: "x")
+  let xNorm2 = LayerNorm(epsilon: 1e-5, axis: [2], elementwiseAffine: false)
+  out = xIn + xChunks[5] .* xFF(xNorm2(out) .* (1 + xChunks[4]) + xChunks[3])
+  let reader: (PythonObject) -> Void = { state_dict in
+    let attn_to_q_weight = state_dict["\(prefix).attn.to_q.weight"].to(
+      torch.float
+    ).cpu().numpy()
+    xToQueries.weight.copy(
+      from: try! Tensor<Float>(numpy: attn_to_q_weight))
+    let attn_to_k_weight = state_dict["\(prefix).attn.to_k.weight"].to(
+      torch.float
+    ).cpu().numpy()
+    xToKeys.weight.copy(
+      from: try! Tensor<Float>(numpy: attn_to_k_weight))
+    let attn_to_v_weight = state_dict["\(prefix).attn.to_v.weight"].to(
+      torch.float
+    ).cpu().numpy()
+    xToValues.weight.copy(
+      from: try! Tensor<Float>(numpy: attn_to_v_weight))
+    let attn_to_out_0_weight = state_dict["\(prefix).attn.to_out.0.weight"].to(
+      torch.float
+    ).cpu().numpy()
+    xUnifyheads.weight.copy(
+      from: try! Tensor<Float>(numpy: attn_to_out_0_weight))
+    let ff_linear_1_weight = state_dict["\(prefix).ff.linear_1.weight"].to(
+      torch.float
+    ).cpu().numpy()
+    xLinear1.weight.copy(
+      from: try! Tensor<Float>(numpy: ff_linear_1_weight))
+    let ff_linear_2_weight = state_dict["\(prefix).ff.linear_2.weight"].to(
+      torch.float
+    ).cpu().numpy()
+    xLinear2.weight.copy(
+      from: try! Tensor<Float>(numpy: ff_linear_2_weight))
+    let ff_out_projection_weight = state_dict["\(prefix).ff.out_projection.weight"].to(
+      torch.float
+    ).cpu().numpy()
+    xOutProjection.weight.copy(
+      from: try! Tensor<Float>(numpy: ff_out_projection_weight))
+    let norm1_linear_weight = state_dict["\(prefix).norm1.linear.weight"]
+      .to(
+        torch.float
+      ).cpu().numpy()
+    for i in 0..<6 {
+      xAdaLNs[i].weight.copy(
+        from: try! Tensor<Float>(
+          numpy: norm1_linear_weight[(k * h * i)..<(k * h * (i + 1)), ...]))
+    }
+  }
+  return (reader, Model([x, c], [out]))
+}
+
 func MMDiT(b: Int, h: Int, w: Int) -> ((PythonObject) -> Void, Model) {
   let x = Input()
   let t = Input()
@@ -504,18 +601,23 @@ func MMDiT(b: Int, h: Int, w: Int) -> ((PythonObject) -> Void, Model) {
     out = blockOut[1]
     readers.append(reader)
   }
-  /*
-  let normFinal = LayerNorm(epsilon: 1e-6, axis: [2], elementwiseAffine: false)
-  let shift = Dense(count: 3072, name: "ada_ln_0")
-  let scale = Dense(count: 3072, name: "ada_ln_1")
-  out = (1 + scale(c)) .* normFinal(out) + shift(c)
-  let linear = Dense(count: 2 * 2 * 16, name: "linear")
-  out = linear(out)
+  out = Functional.concat(axis: 1, context, out)
+  for i in 0..<32 {
+    let (reader, block) = SingleTransformerBlock(
+      prefix: "single_transformer_blocks.\(i)", k: 256, h: 12, b: b, t: 264, hw: h * w,
+      contextBlockPreOnly: i == 31)
+    out = block(out, c)
+    readers.append(reader)
+  }
+  let scale = Dense(count: 3072, name: "ada_ln_0")
+  let shift = Dense(count: 3072, name: "ada_ln_1")
+  out = (1 + scale(c)) .* out + shift(c)
+  let projOut = Dense(count: 2 * 2 * 4, name: "linear")
+  out = projOut(out)
   // Unpatchify
-  out = out.reshaped([b, h, w, 2, 2, 16]).permuted(0, 5, 1, 3, 2, 4).contiguous().reshaped([
-    b, 16, h * 2, w * 2,
+  out = out.reshaped([b, h, w, 2, 2, 4]).permuted(0, 5, 1, 3, 2, 4).contiguous().reshaped([
+    b, 4, h * 2, w * 2,
   ])
-  */
   let reader: (PythonObject) -> Void = { state_dict in
     let x_embedder_proj_weight = state_dict["pos_embed.proj.weight"].to(
       torch.float
@@ -551,35 +653,21 @@ func MMDiT(b: Int, h: Int, w: Int) -> ((PythonObject) -> Void, Model) {
     for reader in readers {
       reader(state_dict)
     }
-    /*
-    let final_layer_adaln_modulation_weight = state_dict[
-      "diffusion_model.final_layer.adaLN_modulation.1.weight"
+    let norm_out_linear_weight = state_dict[
+      "norm_out.linear.weight"
     ].to(
       torch.float
     ).cpu().numpy()
-    let final_layer_adaln_modulation_bias = state_dict[
-      "diffusion_model.final_layer.adaLN_modulation.1.bias"
-    ].to(
-      torch.float
-    ).cpu().numpy()
-    shift.weight.copy(
-      from: try! Tensor<Float>(numpy: final_layer_adaln_modulation_weight[0..<1536, ...]))
-    shift.bias.copy(from: try! Tensor<Float>(numpy: final_layer_adaln_modulation_bias[0..<1536]))
     scale.weight.copy(
-      from: try! Tensor<Float>(numpy: final_layer_adaln_modulation_weight[1536..<(1536 * 2), ...]))
-    scale.bias.copy(
-      from: try! Tensor<Float>(numpy: final_layer_adaln_modulation_bias[1536..<(1536 * 2)]))
-    let final_layer_linear_weight = state_dict["proj_out.weight"].to(
+      from: try! Tensor<Float>(numpy: norm_out_linear_weight[0..<3072, ...]))
+    shift.weight.copy(
+      from: try! Tensor<Float>(numpy: norm_out_linear_weight[3072..<(3072 * 2), ...]))
+    let proj_out_weight = state_dict["proj_out.weight"].to(
       torch.float
     ).cpu().numpy()
-    let final_layer_linear_bias = state_dict["proj_out.bias"].to(
-      torch.float
-    ).cpu().numpy()
-    linear.weight.copy(from: try! Tensor<Float>(numpy: final_layer_linear_weight))
-    linear.bias.copy(from: try! Tensor<Float>(numpy: final_layer_linear_bias))
-    */
+    projOut.weight.copy(from: try! Tensor<Float>(numpy: proj_out_weight))
   }
-  return (reader, Model([x, t, contextIn], [context, out]))
+  return (reader, Model([x, t, contextIn], [out]))
 }
 
 let (reader, dit) = MMDiT(b: 2, h: 64, w: 64)
@@ -593,9 +681,7 @@ graph.withNoGrad {
   dit.compile(inputs: xTensor, tTensor, cTensor)
   reader(state_dict)
   debugPrint(dit(inputs: xTensor, tTensor, cTensor))
-  /*
   graph.openStore("/home/liu/workspace/swift-diffusion/auraflow_v0.1_f32.ckpt") {
     $0.write("dit", model: dit)
   }
-  */
 }
